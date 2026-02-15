73be15abe4b198b5837220e63f043e8a0d43fbd8
DustUnderBag <61408175+DustUnderBag@users.noreply.github.com>
2026-02-14 22:29:31 -0500

    Refactor `deleteItem` to separate logic of cases
    Separated logic of deleting node with no child from node with ONE child.

--- main.js
+++ main.js
@@ -23,21 +23,44 @@ function insert(root , value) {
     return root;
 }
 
+//return the next bigger node, the right sub-tree's smallest.
+function findSuccessor(root) {
+
+}
+
+/*Three cases
+1. Delete leaf node -> 
+    Remove the leaf node directly.
+    -> replace the node with null object.
+
+2. Delete node with 1 child node -> 
+     Connect target's parent node with the child node.
+     -> Return the target's child node to the parent node.
+
+3. Delete node with 2 child nodes ->
+    Step 1: 
+     Inorder successor: FInd the next greater value, which is the smallest value in right subtree OR 
+     Inorder predecessor: Find the next smaller value, which is the largest value in left subtree.
+
+    Step 2:
+     Replace target node with that successor/predecessor node, then delete the replacement node.
+
+*/
 function deleteItem(root, value) {
-    if(value === root.data) {
-        if(!root.left && root.right) {
-            return root.right;
-        }else if(root.left && !root.right) {
-            return root.left;
-        }else if(!root.left && !root.right) {
-            return null;
-        }
-    }
+    //If value doesn't exist in the tree.
+    if(root === null) return root;
 
     if(value < root.data) {
         root.left = deleteItem(root.left, value);
-    }else {
+    }else if(value > root.data ) {
         root.right = deleteItem(root.right, value);
+    }else {
+        //If target node has no child, return null.
+        if(root.left === null && root.right === null) return null;
+
+        //If target node has ONE child, return its child.
+        if(root.left === null) return root.right;
+        if(root.right === null) return root.left;
     }
 
     return root;
@@ -125,8 +148,13 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const tree = buildTree(arr);
-prettyPrint(tree.root);
+insert(tree.root, 20.2)
 
+prettyPrint(tree.root);
+deleteItem(tree.root, 5);
+deleteItem(tree.root, 7);
 deleteItem(tree.root, 1);
+deleteItem(tree.root, 4);
+deleteItem(tree.root, 324);
 
 prettyPrint(tree.root);
